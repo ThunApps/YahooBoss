83d57ba02bd2f7c7a4a9666d43cbd76dd62e3ccf
Fredrik Thunberg <fredrik.thunberg@gmail.com>
2015-07-08 11:00:24 +0200

    Code cleanup

--- yahooboss.go
+++ yahooboss.go
@@ -1,110 +1,119 @@
 package yahooboss
 
 import (
-  "github.com/garyburd/go-oauth/oauth"
-  "net/url"
-  "net/http"
-  "io/ioutil"
-  "log"
-  "fmt"
-  "encoding/json"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+
+	"github.com/garyburd/go-oauth/oauth"
 )
 
+type BossError struct {
+	Type string
+}
+
+func (e *BossError) Error() string {
+	return fmt.Sprintf("Error: %s", e.Type)
+}
+
 type BossSearch struct {
-    Token string
-    Secret string
-    SearchType string
+	Token      string
+	Secret     string
+	SearchType string
 }
 
 type BossResultRow struct {
-  url string
-  title string
+	url   string
+	title string
 }
 
 type BossResult struct {
-    start int
-    end int
-    count int
-    totalresults int
-    results []BossResultRow
+	start        int
+	end          int
+	count        int
+	totalresults int
+	results      []BossResultRow
 }
 
 type BossResponse struct {
-  responsecode int
-  bossresult BossResult
+	responsecode int
+	bossresult   BossResult
 }
 
 func (bs *BossSearch) signQuery(url string, values *url.Values) {
-  cred := oauth.Credentials{}
-  cred.Token = bs.Token
-  cred.Secret = bs.Secret
+	cred := oauth.Credentials{}
+	cred.Token = bs.Token
+	cred.Secret = bs.Secret
 
-  client := oauth.Client{}
-  client.SignatureMethod = oauth.HMACSHA1
-  client.Credentials = cred
+	client := oauth.Client{}
+	client.SignatureMethod = oauth.HMACSHA1
+	client.Credentials = cred
 
-  client.SignForm( nil, "GET", url, *values )
+	client.SignForm(nil, "GET", url, *values)
 }
 
 func (bs *BossSearch) buildQuery(text string) string {
-  search_type := bs.SearchType
-  boss_url := fmt.Sprintf("%s%s","https://yboss.yahooapis.com/ysearch/", search_type)
+	searchType := bs.SearchType
+	bossURL := fmt.Sprintf("%s%s", "https://yboss.yahooapis.com/ysearch/", searchType)
 
-  get_params := url.Values{}
-  get_params.Set("q", fmt.Sprintf("\"%s\"", text))
-  get_params.Add("format", "json")
-  get_params.Add("title", "lyrics")
+	getParams := url.Values{}
+	getParams.Set("q", fmt.Sprintf("\"%s\"", text))
+	getParams.Add("format", "json")
+	getParams.Add("title", "lyrics")
 
-  bs.signQuery(boss_url, &get_params)
-  return fmt.Sprintf("%s?%s", boss_url, get_params.Encode())
+	bs.signQuery(bossURL, &getParams)
+	return fmt.Sprintf("%s?%s", bossURL, getParams.Encode())
 }
 
 /* Todo: Add err */
 func (bs *BossSearch) Search(text string) BossResponse {
-  url := bs.buildQuery(text)
+	url := bs.buildQuery(text)
 
-  res, err := http.Get(url)
-  if err != nil {
+	res, err := http.Get(url)
+	if err != nil {
 		log.Fatal(err)
 	}
 
-  defer res.Body.Close()
+	defer res.Body.Close()
 
-  body, err := ioutil.ReadAll(res.Body)
-  if err != nil {
-    panic(err)
-  }
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
 
-  return parseSearchResult(body, bs.SearchType)
+	return parseSearchResult(body, bs.SearchType)
 }
 
 /* Todo: Add err */
-func parseSearchResult(body []byte, search_type string) BossResponse {
-  var dat map[string]interface{}
+func parseSearchResult(body []byte, searchType string) BossResponse {
+	var dat map[string]interface{}
 
-  if err := json.Unmarshal(body, &dat); err != nil {
-    panic(err)
-  }
-  boss_response := dat["bossresponse"].(map[string]interface{})
-  search_type_results := boss_response[search_type].(map[string]interface{})
-  results := search_type_results["results"].([]interface{})
-  var br BossResponse
-  var bresult BossResult
-  var brows []BossResultRow
-  for _, value := range results {
-    map_value := value.(map[string]interface{})
-    url := map_value["url"].(string)
-    title := map_value["title"].(string)
+	if err := json.Unmarshal(body, &dat); err != nil {
+		panic(err)
+	}
+	bossResponse := dat["bossresponse"].(map[string]interface{})
+	searchTypeResults := bossResponse[searchType].(map[string]interface{})
+	results := searchTypeResults["results"].([]interface{})
+	var br BossResponse
+	var bresult BossResult
+	var brows []BossResultRow
+	for _, value := range results {
+		mapValue := value.(map[string]interface{})
+		url := mapValue["url"].(string)
+		title := mapValue["title"].(string)
 
-    result := BossResultRow{url, title}
-    brows = append(brows, result)
-  }
-  bresult.start = 0
-  bresult.end = 50
-  bresult.totalresults = 147
-  bresult.results = brows
+		result := BossResultRow{url, title}
+		brows = append(brows, result)
+	}
+	bresult.start = 0
+	bresult.end = 50
+	bresult.totalresults = 147
+	bresult.results = brows
 
-  br.bossresult = bresult
-  br.responsecode = 200
-  return br
+	br.bossresult = bresult
+	br.responsecode = 200
+	return br
 }
